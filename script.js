8f82d6f3c4a964d0e240a2a9b76101c20d377d76
Pavlo <E:\the_bat>
2021-03-09 17:26:29 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,56 @@
+//filled inputs add to Array as object
+//count all object from array
+//add "+" button to add new field to count
+// add "x" button do delete object from array
+
+let newElement = [
+    {name: tagName, type: type, id: id, className: class, inner: innerHTML, holder: placeholder } // переписать все из  20 строки
+]
+
+const createHTMLElement = (tagName, type, id, className, innerHTML, placeholder) => {
+    let element = document.createElement(tagName);
+    element.id = id || "";
+    element.className = className || "";
+    element.innerHTML = innerHTML || "";
+
+    return element;
+}
+
+
+newElement.forEach(element => {
+    let inputWidth = createHTMLElement ("input", "number", element.idWidth, "", "", "width");
+    let inputHeight = createHTMLElement ("input", "height", element.idHeight, "", "", "width");
+    let inputQuantity = createHTMLElement ("input", "number", element.idQuantity, "", "", "Quantity");
+    let inputMaterial = createHTMLElement ("input", "chekbox", element.idMaterial);
+    let inputCut = createHTMLElement ("input", "chekbox", element.idCut);
+    let formDiv = createHTMLElement ("div", "", "", "form");
+
+    formDiv.append(inputWidth, inputHeight, inputQuantity, inputMaterial, inputCut);
+    
+    
+}
+
+// const addToHTML = (e) =>{
+//     document.getElementById('wrapper').appendChild(e);
+// }
+
+class StickerCount {
+    constructor(width, height, quantity, cut, material) {
+        this.width = width;
+        this.height = height;
+        this.quantity = quantity;
+        this.cut = cut;
+        this.material = material;
+    }
+
+    array = Array();
+
+
+}
+
+document.getElementById("addElement").addEventListener('click', function (e) {
+    e.preventDefault();
+    createFormItem();
+});
+
+
